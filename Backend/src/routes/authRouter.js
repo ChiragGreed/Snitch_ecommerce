1ba1eab63750f2a29065e737e5737594cfe20a57
Chirag Greed <chiraggreed02@gmail.com>
2026-07-14 21:24:33 +0530

    feat: Add passport and passport-google-oauth20 dependencies for authentication

--- Backend/src/routes/authRouter.js
+++ Backend/src/routes/authRouter.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { forgotPassword, getMe, googleAuth, login, logout, protectedRoute, register, resetPassword, sessionProtectedRoute } from '../controllers/authController.js';
 import { loginValidator, newPasswordValidator, registerValidator } from '../validation/authValidation.js';
-import passport from 'passport';
 import { verifySessionId, verifyToken } from '../middlewares/authMiddleware.js';
 
 const authRouter = express.Router();
