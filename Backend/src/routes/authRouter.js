b092b035a723be3f9c9eabf1b27f46c202e29261
Chirag Greed <chiraggreed02@gmail.com>
2026-07-14 21:26:38 +0530

    fix: Correctly import passport in authRouter

--- Backend/src/routes/authRouter.js
+++ Backend/src/routes/authRouter.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import passport from 'passport'
 import { forgotPassword, getMe, googleAuth, login, logout, protectedRoute, register, resetPassword, sessionProtectedRoute } from '../controllers/authController.js';
 import { loginValidator, newPasswordValidator, registerValidator } from '../validation/authValidation.js';
 import { verifySessionId, verifyToken } from '../middlewares/authMiddleware.js';
